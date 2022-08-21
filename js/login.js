df1c96b13d843ff459b295cf6495291ffb305661
Emmanuel <emmanuelsuareztou@gmail.com>
2022-08-21 18:45:06 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,26 @@
+function datos(){
+let persona = {};
+persona.email = document.getElementById('email').value;
+persona.password = document.getElementById('password').value;
+return persona;
+}
+
+function validar () {
+let u = datos();
+if (u.email == "" || u.password == "") {
+alert("Los campos no pueden estar vacios!");
+}
+else {
+    alert ("Bienvenido");
+    window.location.href = "/portada.html";
+}
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    document.getElementById('bttn').addEventListener('click', ()=>{
+
+        validar();
+    })
+
+})
