ebb0eff46756a10c7165f0c36a9dcd1375d804de
Emmanuel <emmanuelsuareztou@gmail.com>
2022-08-28 12:18:51 -0300

    cerrar sesion

--- js/index.js
+++ js/index.js
@@ -1,29 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.getElementById('sesion').innerHTML = `Hola <strong>${localStorage.getItem('usuario')}</strong>!`
-
-    document.getElementById('cerrarSesion').addEventListener('click', ()=>{
-
-        Swal.fire({
-            icon: 'info',
-            title: 'Estas seguro?',
-            showDenyButton: true,
-            confirmButtonText: 'Si',
-            denyButtonText: `No`,
-            background: '#485c8b',
-            color: '#fff',
-            confirmButtonColor: '#e98b20',
-            denyButtonColor: '#e8a75d',
-          }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = './';
-                localStorage.removeItem(usuario);
-            }
-          })
-
-
-    })
-
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
---
+++ js/name.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    document.getElementById('sesion').innerHTML = `Hola <strong>${localStorage.getItem('usuario')}</strong>!`
+    
+    document.getElementById('cerrarSesion').addEventListener('click', ()=>{
+
+        Swal.fire({
+            icon: 'info',
+            title: 'Estas seguro?',
+            showDenyButton: true,
+            confirmButtonText: 'Si',
+            denyButtonText: `No`,
+            background: '#485c8b',
+            color: '#fff',
+            confirmButtonColor: '#e98b20',
+            denyButtonColor: '#e8a75d',
+          }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = './';
+                localStorage.removeItem(usuario);
+            }
+          })
+
+
+    })
+
+
+
+})
