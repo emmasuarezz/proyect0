42eb036d12f541cd368861d3b188f41c731eab47
Emmanuel <emmanuelsuareztou@gmail.com>
2022-09-16 19:29:01 -0300

    entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,16 +1,80 @@
 let ProductArray = [];
+let CommentsArray = [];
+let ImagesArray = [];
+
+
+function showImgGrande(array){
+    let i = localStorage.getItem('foto');
+    let htmlContentToAppend = "";
+    let cuadro = document.getElementById('cuadro');
+
+    htmlContentToAppend += `
+
+    <div class="img-fluid"">
+    <img class="d-block w-100" src="${array[i]}" alt="">
+    </div>
+    
+    `;
+
+    cuadro.innerHTML = htmlContentToAppend;
+}
+
+function setImageIndex(index){
+
+    localStorage.setItem('foto', index);
+
+
+
+}
+
+
+
 
 
 function showCurrentProductImage(array){
 
     let htmlContentToAppend = "";
-            htmlContentToAppend += `
-                        <img src="${array.images[0]}" alt="${array.description}" style="float: right;">
-            `
-        
+    let index = 0;
+    for(let i = 0; i < array.length; i++){
+        let imagen = array[i];
 
-        document.getElementById("prueba").innerHTML += htmlContentToAppend;
+            htmlContentToAppend += `
+            <div class="col-lg-3 col-md-12 mb-2 mb-lg-0">
+            <img
+              src="${imagen}"
+              class="w-100 shadow-1-strong rounded mb-4"
+              alt="${array.name}"
+              onclick="setImageIndex(${index}); showImgGrande(ImagesArray);"
+            />
+          </div>
+            `           
+        index = index +1;
     }
+    document.getElementById("imagenes").innerHTML += htmlContentToAppend;
+}
+
+function showCurrentProductComments(array){
+
+    let htmlContentToAppend = "";
+    for(let comment of array){
+
+            htmlContentToAppend += `
+            <div class="card-body cartas">
+              <p>${comment.description}</p>
+  
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <p class="small mb-0 ms-2"><strong>${comment.user}</strong></p>
+                </div>
+                <div class="d-flex flex-row align-items-center">
+                  <p class="small text mb-0">${comment.dateTime}</p>
+                </div>
+              </div>
+            </div>
+            `           
+    }
+    document.getElementById("comentarios").innerHTML += htmlContentToAppend;
+}
 
 
 
@@ -18,16 +82,22 @@ function showCurrentProduct(array){
 
         let htmlContentToAppend = "";
                 htmlContentToAppend += `
-                        <div>
-                            <p class="flecha" > < <span id="atras" class="category"><a onclick="window. history. back();">${array.category}</a></span></p>
-                            <h1 class="nombre"><strong>${array.name}</strong></h1>
-                            <hr>
-                            <p class="description"> ${array.description} </p>
-                            <h2 class="precio">${array.currency} ${array.cost} </h2>
-                            <p class="vendidos">${array.soldCount} vendidos</p>
-                        </div>
+                        
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+          <h1 id="titulo" class="display-4 font-weight-normal">${array.name}</h1>
+          <p class="lead font-weight-normal">${array.description}</p>
+          <p class="lead font-weight-normal">${array.currency} ${array.cost}</p>
+          <a class="btn btn-outline-secondary" href="#">Comprar</a>
+          </br>
+          </br>
+          <a class="lead font-weight-normal tituloCOM" href="#imagenes">Imagenes</a>
+          </br>
+          </br>
+          <a class="lead font-weight-normal tituloCOM" href="#comentarios">Reseñas</a>
+        </div>
+      </div>
                 `
-            document.getElementById("prueba2").innerHTML = htmlContentToAppend;
+            document.getElementById("main").innerHTML += htmlContentToAppend;
         }
 
 
@@ -38,15 +108,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
     getJSONData("https://japceibal.github.io/emercado-api/products/" + prodID + ".json").then(function(resultObj){
         if (resultObj.status === "ok"){
-            ProductArray = resultObj.data
-            showCurrentProductImage(ProductArray);
+            ProductArray = resultObj.data;
+            ImagesArray = resultObj.data.images;
             showCurrentProduct(ProductArray);
+            showCurrentProductImage(ImagesArray);
+            showImgGrande(ProductArray);
 
         }
     });
 
+    getJSONData("https://japceibal.github.io/emercado-api/products_comments/" + prodID + ".json").then(function(resultObj){
+
+        if (resultObj.status === "ok"){
+            CommentsArray = resultObj.data;
 
 
+        }
+    
+    
+});
+
+    
 
     });
 
